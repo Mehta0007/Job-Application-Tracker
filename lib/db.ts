6bebf64f30510d8b3af7417e45ed98da3e2433ed
Ankit Mehta <ankitmmehta08@gmail.com>
2026-01-26 10:43:30 +0530

    feat: polish landing UI and improve MongoDB connection setup

---
+++ lib/db.ts
@@ -0,0 +1,46 @@
+ import mongoose from "mongoose";
+
+
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+    throw new Error(
+        "Please define the MONGODB_URI env variable inside .env"
+    )
+}
+
+let cached = (global as any).mongoose
+
+if (!cached) {
+    cached = (global as any).mongoose = {
+        conn: null,
+        promise: null,
+    };
+}
+
+export async function connectDB(){
+    // “Are We Already Connected?”
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+// “Is Someone Already Connecting?”
+    if (!cached.promise) {
+// “Start Connecting”
+        cached.promise = mongoose
+        .connect(MONGODB_URI,{
+            bufferCommands: false,
+        })
+        .then((mongoose) => mongoose)
+    }
+    // “Wait Until Connected”
+     cached.conn = await cached.promise;
+// “Return Connection”
+     return cached.conn
+
+
+}
+
+
+
